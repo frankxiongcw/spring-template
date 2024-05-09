49d3e6cf811ca05e4f244a50f4faaba9aeafb0b4
熊灿伟 <frank.xiongcw@hotmail.com>
2024-05-09 14:00:16 +0800

    优化自动生成代码

---
+++ template-service/src/main/java/com/template/core/dao/LiquidationDetailMapper.java
@@ -0,0 +1,16 @@
+package com.template.core.dao;
+
+import com.template.core.entity.LiquidationDetail;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 清算明细 Mapper 接口
+ * </p>
+ *
+ * @author xiong.canwei
+ * @since 2024-05-09
+ */
+public interface LiquidationDetailMapper extends BaseMapper<LiquidationDetail> {
+
+}
---
+++ template-service/src/main/java/com/template/core/service/LiquidationDetailService.java
@@ -0,0 +1,72 @@
+package com.template.core.service;
+
+import com.template.core.entity.LiquidationDetail;
+import com.template.core.dao.LiquidationDetailMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.template.core.entity.User;
+import com.template.core.utils.*;
+import com.template.api.pojo.dto.LiquidationDetailQueryDTO;
+import com.template.api.pojo.dto.LiquidationDetailDTO;
+import com.template.api.pojo.vo.LiquidationDetailVO;
+import com.template.api.pojo.PageResult;
+import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import org.springframework.transaction.annotation.Transactional;
+import com.github.pagehelper.PageHelper;
+import javax.annotation.Resource;
+import java.util.List;
+/**
+ * <p>
+ * 清算明细 服务实现类
+ * </p>
+ *
+ * @author xiong.canwei
+ * @since 2024-05-09
+ */
+@Service
+public class LiquidationDetailService extends ServiceImpl<LiquidationDetailMapper, LiquidationDetail> {
+
+    @Resource
+    private LiquidationDetailMapper liquidationDetailMapper;
+
+
+    public PageResult<LiquidationDetailVO> listByPage (LiquidationDetailQueryDTO queryDTO) {
+        PageHelper.startPage(queryDTO.getPageNum(), queryDTO.getPageSize(), true);
+        LambdaQueryWrapper<LiquidationDetail> wrapper = new LambdaQueryWrapper<>();
+
+        List<LiquidationDetail> list = liquidationDetailMapper.selectList(wrapper);
+        return PageResultUtil.pageResult(list, LiquidationDetailVO.class); 
+    }
+
+    public LiquidationDetailVO detail (Long id) {
+        LiquidationDetail detail = liquidationDetailMapper.selectById(id);
+        return BeanUtils.copyProperties(detail, LiquidationDetailVO.class);
+    }
+
+    @Transactional
+    public Boolean save(LiquidationDetailDTO saveDTO){
+        LiquidationDetail liquidationDetail = BeanUtils.copyProperties(saveDTO, LiquidationDetail.class);
+        EntityUserUtil.addCreateAndModifyUser(liquidationDetail,new User());
+        save(liquidationDetail);
+        return true;
+    }
+
+    @Transactional
+    public Boolean update(LiquidationDetailDTO updateDTO){
+        LiquidationDetail liquidationDetail = BeanUtils.copyProperties(updateDTO, LiquidationDetail.class);
+        EntityUserUtil.addModifyUser(liquidationDetail,new User());
+        saveOrUpdate(liquidationDetail);
+        return true;
+    }
+
+    @Transactional
+    public Boolean delete(Long id){
+        LiquidationDetail detail = liquidationDetailMapper.selectById(id);
+        LambdaUpdateWrapper<LiquidationDetail> updateWrapper = new LambdaUpdateWrapper<>();
+//        updateWrapper.set(LiquidationDetail::getDeleteFlag,Boolean.TRUE).eq(LiquidationDetail::getId,id);
+        update(updateWrapper);
+        return true;
+    }
+}
